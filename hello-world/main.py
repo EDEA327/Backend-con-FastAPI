17bc239992b936d27d54e3b9b2b43d42420f2857
Erick Escobar <edeajob27@gmail.com>
2022-09-02 10:26:53 -0500

    Validations Query

--- hello-world/main.py
+++ hello-world/main.py
@@ -3,7 +3,7 @@ from typing import Dict,Optional
 #pydantic
 from pydantic import BaseModel
 #FastAPI
-from fastapi import FastAPI,Body
+from fastapi import FastAPI,Body,Query
 # Models
 class Person(BaseModel):
     name: str
@@ -17,7 +17,13 @@ app:FastAPI = FastAPI()
 def home() -> Dict:
     return {"Hello": "world"}
 # Request and response body
-
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+# Validations of Query parameters
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None,min_length=1,max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
