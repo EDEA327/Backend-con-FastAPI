00d76404b7a6a7fcc37f87a47ac77983e2a8bbf8
Erick Escobar <edeajob27@gmail.com>
2022-09-02 09:48:25 -0500

    Models

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,15 +1,23 @@
-# Dependencias del proyecto
-from fastapi import FastAPI
-from typing import Dict
-# Instanciando la clase FastAPI
+#Python
+from typing import Dict,Optional
+#pydantic
+from pydantic import BaseModel
+#FastAPI
+from fastapi import FastAPI,Body
+# Models
+class Person(BaseModel):
+    name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str]
+    is_married: Optional[bool]
 app:FastAPI = FastAPI()
-
+#Metodos
 @app.get("/")
 def home() -> Dict:
     return {"Hello": "world"}
-
 # Request and response body
 
 @app.post("/person/new")
-def create_person():
-    pass
+def create_person(person: Person = Body(...)):
+    return person
