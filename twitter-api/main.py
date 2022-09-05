ebe55edc34663b1344c305dc8f9c1fbc6e955533
Erick Escobar <edeajob27@gmail.com>
2022-09-05 11:14:36 -0500

    tweet_models

--- twitter-api/main.py
+++ twitter-api/main.py
@@ -1,6 +1,6 @@
 # Python
 from uuid import UUID
-from datetime import date
+from datetime import date,datetime
 from typing import Optional
 # Pydantic
 from pydantic import BaseModel
@@ -37,7 +37,15 @@ class User(UserBase):
     birth_date: Optional[date] = Field(default=None)
 
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+        )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by:User = Field(...)
 
 @app.get(path='/' )
 def home():
